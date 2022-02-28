a07a21097801bb62ff3ba4194dd845357503c2a3
EC2 Default User <skgus2624@gmail.com>
2022-02-28 08:58:22 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -47,7 +47,7 @@ class MoviesController < ApplicationController
   
   if params[:sort_key] == 'title'
     #@movie = Movie.order('title').all
-    @movie = Movie.order(:sort_key).all
+    @movie = Movie.order(@sort_key).all
   end
     
 end
