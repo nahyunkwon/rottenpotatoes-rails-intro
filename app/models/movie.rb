ac6d736237f38846d8fd2741368020a1bf8f8e52
EC2 Default User <skgus2624@gmail.com>
2022-03-09 02:48:46 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        Movie.select(:rating).map(&:rating).uniq
+        return Movie.select(:rating).map(&:rating).uniq
     end
     
-    def self.with_ratings (ratings)
-        Movie.where(rating: ratings)
+    def self.with_ratings(ratings)
+        return Movie.where(rating: ratings)
     end
 end
